894f124f3630867ef3e628315062629826800c14
Felix Soubelet <felix.soubelet@protonmail.com>
2020-10-09 09:58:00 +0200

    feature(github actions): #238 remove unused import

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
 import pathlib
 import sys
 # ignore numpy warnings, see:
