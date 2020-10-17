970b7c99ff29409b58c5c4ba5967b82cfded7032
Felix Soubelet <felix.soubelet@protonmail.com>
2020-10-17 15:49:05 +0200

    #238 pathlib iotools

--- omc3/utils/iotools.py
+++ omc3/utils/iotools.py
@@ -13,9 +13,9 @@ Feel free to use and extend this module.
 
 """
 import json
-import os
 import shutil
 from pathlib import Path
+from typing import List, Union
 
 from generic_parser.entry_datatypes import get_instance_faker_meta
 from generic_parser.entrypoint_parser import save_options_to_config
@@ -31,57 +31,60 @@ def delete_content_of_dir(path_to_dir):
     Deletes all folders, files and symbolic links in given directory.
     :param string path_to_dir:
     """
-    if not os.path.isdir(path_to_dir):
+    directory = Path(path_to_dir)
+    if not directory.is_dir():
         return
-
-    for item in os.listdir(path_to_dir):
-        item_path = os.path.join(path_to_dir, item)
-        delete_item(item_path)
+    for item in directory.iterdir():
+        delete_item(item)
 
 
-def delete_item(path_to_item):
+def delete_item(path_to_item) -> None:
     """ Deletes the item given by path_to_item. It distinguishes between a file, a directory and a
     symbolic link.
     """
+    item = Path(path_to_item)
     try:
-        if os.path.isfile(path_to_item):
-            os.unlink(path_to_item)
-        elif os.path.isdir(path_to_item):
-            shutil.rmtree(path_to_item)
-        elif os.path.islink(path_to_item):
-            os.unlink(path_to_item)
+        if item.is_file() or item.is_symlink():
+            item.unlink()
+        elif item.is_dir():
+            shutil.rmtree(item)
+    except FileNotFoundError:
+        LOG.error(f"No item to delete at '{item}'")
     except OSError:
-        LOG.error("Could not delete item because of OSError. Item: '{}'".format(path_to_item))
+        LOG.error(f"Could not delete item '{item}'")
 
 
-def copy_content_of_dir(src_dir, dst_dir):
+def copy_content_of_dir(src_dir, dst_dir) -> None:
     """ Copies all files and directories from src_dir to dst_dir. """
-    if not os.path.isdir(src_dir):
+    source = Path(src_dir)
+    destination = Path(dst_dir)
+
+    if not source.is_dir():
         return
 
-    create_dirs(dst_dir)
+    create_dirs(destination)
 
-    for item in os.listdir(src_dir):
-        src_item = os.path.join(src_dir, item)
-        dst_item = os.path.join(dst_dir, item)
-        copy_item(src_item, dst_item)
+    for element in source.iterdir():
+        copy_item(element, destination)
 
 
-def create_dirs(path_to_dir) -> None:
-    """ Creates all dirs to path_to_dir if not exists. """
-    path_to_dir = Path(path_to_dir)
-    if not path_to_dir.exists():
-        path_to_dir.mkdir()
-        LOG.debug(f"Created directory structure: '{path_to_dir}'")
+def create_dirs(directory) -> None:
+    """ Creates all dirs to directory if not exists. """
+    directory = Path(directory)
+    if not directory.exists():
+        directory.mkdir()
+        LOG.debug(f"Created directory at: '{directory}'")
 
 
-def copy_item(src_item, dest):
+def copy_item(src_item, dest) -> None:
     """ Copies a file or a directory to dest. dest may be a directory.
     If src_item is a directory then all containing files and dirs will be copied into dest. """
+    source = Path(src_item)
+    destination = Path(dest)
     try:
-        if os.path.isfile(src_item):
+        if source.is_file():
             shutil.copy2(src_item, dest)
-        elif os.path.isdir(src_item):
+        elif source.is_dir():
             copy_content_of_dir(src_item, dest)
         else:
             raise IOError
@@ -89,149 +92,121 @@ def copy_item(src_item, dest):
         LOG.error("Could not copy item because of IOError. Item: '{}'".format(src_item))
 
 
-def deleteFilesWithoutGitignore(pathToDirectory):
+def delete_files_except_gitignore(path_to_directory: Union[Path, str]) -> bool:
     """
-    Deletes all files in the given pathToDirectory except of the file with the name '.gitignore'
+    Deletes all files in the given path_to_directory except of the file with the name '.gitignore'
 
-    :returns: bool -- True if the directory exists and the files are deleted otherwise False.
+    Args:
+        path_to_directory (Union[Path, str]): Path to the directory to wipe.
+
+    Returns:
+        True if the directory exists and the files are deleted otherwise False
     """
-    if not os.path.exists(pathToDirectory):
+    directory = Path(path_to_directory)
+    if not directory.is_dir():
         return False
 
-    filenames_list = os.listdir(pathToDirectory)
-
-    for filename in filenames_list:
-        if ".gitignore" != filename:
-            os.remove( os.path.join(pathToDirectory,filename) )
-
+    for element in directory.iterdir():
+        if element.is_file() and element.name != ".gitignore":
+            element.unlink()
     return True
 
 
-def exists_directory(path_to_dir):
-    return os.path.isdir(path_to_dir)
+def exists_directory(path_to_dir) -> bool:
+    return Path(path_to_dir).is_dir()
 
 
-def not_exists_directory(path_to_dir):
+def not_exists_directory(path_to_dir) -> bool:
     return not exists_directory(path_to_dir)
 
 
-def no_dirs_exist(*dirs):
+def no_dirs_exist(*dirs) -> bool:
     return not dirs_exist(*dirs)
 
 
-def dirs_exist(*dirs):
-    for d in dirs:
-        if not os.path.isdir(d):
+def dirs_exist(*dirs) -> bool:
+    for directory in dirs:
+        if not Path(directory).is_dir():
             return False
     return True
 
 
-def get_all_filenames_in_dir_and_subdirs(path_to_dir):
+def get_all_filenames_in_dir_and_subdirs(path_to_dir) -> List[Path]:
     """ Looks for files(not dirs) in dir and subdirs and returns them as a list.  """
-    if not os.path.isdir(path_to_dir):
-        return []
-    result = []
-    for root, sub_folders, files in os.walk(path_to_dir):  # @UnusedVariable
-        result += files
-    return result
+    return [element for element in Path(path_to_dir).rglob("*") if element.is_file()]
 
 
-def get_all_absolute_filenames_in_dir_and_subdirs(path_to_dir):
-    """ Looks for files(not dirs) in dir and subdirs and returns them as a list.  """
-    if not os.path.isdir(path_to_dir):
-        return []
-    abs_path_to_dir = os.path.abspath(path_to_dir)
-    result = []
-    for root, sub_folders, files in os.walk(abs_path_to_dir):  # @UnusedVariable
-        for file_name in files:
-            result.append(os.path.join(root, file_name))
-    return result
-
-
-def get_all_filenames_in_dir(path_to_dir):
+def get_all_filenames_in_dir(path_to_dir) -> List[Path]:
     """ Looks for files in dir(not subdir) and returns them as a list """
-    if not os.path.isdir(path_to_dir):
-        return []
-    result = []
-    for item in os.listdir(path_to_dir):
-        item_path = os.path.join(path_to_dir, item)
-        if os.path.isfile(item_path):
-            result.append(item)
-    return result
+    return [element for element in Path(path_to_dir).iterdir() if element.is_file()]
 
 
-def get_all_dir_names_in_dir(path_to_dir):
+def get_all_dir_names_in_dir(path_to_dir) -> List[Path]:
     """ Looks for directories in dir and returns them as a list """
-    if not os.path.isdir(path_to_dir):
-        return []
-    result = []
-    for item in os.listdir(path_to_dir):
-        item_path = os.path.join(path_to_dir, item)
-        if os.path.isdir(item_path):
-            result.append(item)
-    return result
+    return [element for element in Path(path_to_dir).iterdir() if element.is_dir()]
 
 
-def is_empty_dir(directory):
-    return 0 == os.listdir(directory)
+def is_not_empty_dir(directory) -> bool:
+    return len(Path(directory).iterdir() != 0)
 
 
-def is_not_empty_dir(directory):
-    return not is_empty_dir(directory)
-
-
-def read_all_lines_in_textfile(path_to_textfile):
-    if not os.path.exists(path_to_textfile):
-        LOG.error("File does not exist: '{}'".format(path_to_textfile))
+def read_all_lines_in_textfile(path_to_textfile) -> str:
+    textfile = Path(path_to_textfile)
+    if not textfile.exists() or not textfile.is_file():
+        LOG.error(f"No file at location '{textfile}'")
         return ""
-    with open(path_to_textfile) as textfile:
-        return textfile.read()
+    return textfile.read_text()
 
 
-def append_string_to_textfile(path_to_textfile, str_to_append):
+def append_string_to_textfile(path_to_textfile, str_to_append) -> None:
     """ If file does not exist, a new file will be created. """
-    with open(path_to_textfile, "a") as file_to_append:
+    with Path(path_to_textfile).open("a") as file_to_append:
         file_to_append.write(str_to_append)
 
 
-def write_string_into_new_file(path_to_textfile, str_to_insert):
+def write_string_into_new_file(path_to_textfile, str_to_insert) -> None:
     """ An existing file will be truncated. """
-    with open(path_to_textfile, "w") as new_file:
-        new_file.write(str_to_insert)
+    Path(path_to_textfile).write_text(str_to_insert)
 
 
-def replace_keywords_in_textfile(path_to_textfile, dict_for_replacing, new_output_path=None):
+def replace_keywords_in_textfile(
+    path_to_textfile: Union[Path, str],
+    dict_for_replacing: dict,
+    new_output_path: Union[Path, str] = None,
+) -> None:
     """
-    This function replaces all keywords in a textfile with the corresponding values in the dictionary.
-    E.g.: A textfile with the content "%(This)s will be replaced!" and the dict {"This":"xyz"} results
-    to the change "xyz will be replaced!" in the textfile.
+    Replaces all keywords in a textfile with the corresponding values in the dictionary.
+    E.g.: A textfile with the content "%(This)s will be replaced!" and the dict {"This": "xyz"}
+    results to the change "xyz will be replaced!" in the textfile.
 
     Args:
-        new_output_path: If new_output_path is None, then the source file will be replaced.
+        path_to_textfile (Union[Path, str]): Path to the file containing text in which to replace
+            elements.
+        dict_for_replacing (dict): dictionary of elements to replace and the values to replace
+            them with.
+        new_output_path: Optional path to another file in which to write the result. If not
+            provided, then the source file's content will be replaced.
     """
-    if new_output_path is None:
-        destination_file = path_to_textfile
-    else:
-        destination_file = new_output_path
-
+    destination_file = path_to_textfile if new_output_path is None else new_output_path
     all_lines = read_all_lines_in_textfile(path_to_textfile)
     lines_with_replaced_keys = all_lines % dict_for_replacing
     write_string_into_new_file(destination_file, lines_with_replaced_keys)
 
 
-def json_dumps_readable(json_outfile, object_to_dump):
+def json_dumps_readable(json_outfile, object_to_dump) -> None:
     """ This is how you write a beautiful json file
     
     Args:
         json_outfile: File to write
         object_to_dump: object to dump to json format
     """
-    object_to_dump = json.dumps(object_to_dump).replace(", ", ",\n    "
-                                                        ).replace("[", "[\n    "
-                              ).replace("],\n    ", "],\n\n"
-                              ).replace("{", "{\n"
-                              ).replace("}", "\n}")
-    with open(json_outfile, "w") as json_file:
+    object_to_dump = json.dumps(object_to_dump)\
+        .replace(", ", ",\n    ")\
+        .replace("[", "[\n    ")\
+        .replace("],\n    ", "],\n\n")\
+        .replace("{", "{\n")\
+        .replace("}", "\n}")
+    with Path(json_outfile).open("w") as json_file:
         json_file.write(object_to_dump)
 
 
